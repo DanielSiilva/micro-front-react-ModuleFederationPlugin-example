60fe6a25c4b1814f468584c1ef33c8507ef55b0b
DanielSiilva <daniellsousa0614@gmail.com>
2024-04-05 11:03:25 -0300

    feat: app-container

---
+++ app-container/src/App.tsx
@@ -0,0 +1,24 @@
+import Button01 from "remoteApp/Button";
+import Button02 from "remoteApp1/Button";
+
+function App() {
+  return (
+    <div className="border-4 border-red-300 flex flex-col justify-center h-80">
+      <div className="w-full text-center">APP CONTAINTER</div>
+      <div className="flex h-full w-full justify-center items-center gap-3">
+        <div className="basis-1/3 border-purple-700 border-2">
+          <div className="text-center w-full">APP1 - React</div>
+          <div className="my-3 mx-2">APP1: Carregando conteudo ...</div>
+          <Button01 />
+        </div>
+        <div className="basis-1/3 border-cyan-700 border-2">
+          <div className="text-center w-full">APP2 - React</div>
+          <div className="my-3 mx-2">APP2: Carregando conteudo ... </div>
+          <Button02 />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
